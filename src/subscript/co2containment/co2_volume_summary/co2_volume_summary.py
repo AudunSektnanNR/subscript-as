8c5ef28bf2703dd0d28abc8edfdcb4217d8c75c8
jorgesicachanr <114475076+jorgesicachanr@users.noreply.github.com>
2023-04-18 10:27:04 +0200

    Update co2_volume_summary.py

--- src/subscript/co2containment/co2_volume_summary/co2_volume_summary.py
+++ src/subscript/co2containment/co2_volume_summary/co2_volume_summary.py
@@ -12,7 +12,7 @@ import shapely.geometry
 from subscript.co2containment.co2_mass_calculation.co2_mass_calculation import calculate_co2_volume
 from subscript.co2containment.co2_mass_calculation.co2_mass_calculation import Co2VolumeData
 
-from .calculate import (
+from subscript.co2containment.co2_mass_calculation.calculate import (
     calculate_co2_containment_vol,
     ContainedCo2Vol,
 )
