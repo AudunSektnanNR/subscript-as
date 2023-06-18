41320b647581e90a9fe13f5192bf0bcc4ae9bf27
AudunSektnanNR <sek@nr.no>
2023-06-18 16:49:08 +0200

    Some refactoring.

--- src/subscript/co2containment/co2_calculation.py
+++ src/subscript/co2containment/co2_calculation.py
@@ -13,8 +13,8 @@ DEFAULT_WATER_DENSITY = 1000.0
 TRESHOLD_SGAS = 1e-16
 TRESHOLD_AMFG = 1e-16
 
-PROPERTIES_USED_FOR_CALCULATIONS = ["RPORV", "PORV", "SGAS", "DGAS", "BGAS", "DWAT",
-                                    "BWAT", "AMFG", "YMFG", "XMF2", "YMF2"]
+PROPERTIES_TO_EXTRACT = ["RPORV", "PORV", "SGAS", "DGAS", "BGAS", "DWAT",
+                         "BWAT", "AMFG", "YMFG", "XMF2", "YMF2"]
 
 
 class CalculationType(Enum):
@@ -54,9 +54,9 @@ class Co2DataAtTimeStep:
     date: str
     aqu_phase: Optional[np.ndarray]
     gas_phase: Optional[np.ndarray]
-    volume_coverage: Optional[np.ndarray] = None
+    volume_coverage: Optional[np.ndarray] = None  # NBNB-AS: Rename?
 
-    def total_amount(self) -> np.ndarray:
+    def total_amount(self) -> np.ndarray:  # NBNB-AS: No longer used
         return self.aqu_phase + self.gas_phase
 
 
@@ -90,10 +90,10 @@ def _read_props(
 
 def _fetch_properties(
         unrst: EclFile,
-        prop_names: List
+        properties_to_extract: List  # NBNB-AS
 ) -> Tuple[Dict[str, List[np.ndarray]], List[str]]:
     dates = [d.strftime("%Y%m%d") for d in unrst.report_dates]
-    properties = _read_props(unrst, prop_names)
+    properties = _read_props(unrst, properties_to_extract)
     properties = {p: {d[1]: properties[p][d[0]].numpy_copy()
                       for d in enumerate(dates)}
                   for p in properties}
@@ -121,15 +121,15 @@ def _is_subset(first: List[str], second: List[str]) -> bool:
 def _extract_source_data(
         grid_file: str,
         unrst_file: str,
-        props: List[str],
+        properties_to_extract: List[str],
         init_file: Optional[str] = None,
         zone_file: Optional[str] = None
 ) -> SourceData:
     print("Start extracting source data")
     grid = EclGrid(grid_file)
     unrst = EclFile(unrst_file)
-    init = EclFile(init_file)
-    properties, dates = _fetch_properties(unrst, props)
+    init = EclFile(init_file)  # NBNB-AS: What if init_file == None?
+    properties, dates = _fetch_properties(unrst, properties_to_extract)
     print("Done fetching properties")
 
     active = np.where(grid.export_actnum().numpy_copy() > 0)[0]
@@ -139,7 +139,7 @@ def _extract_source_data(
     elif _is_subset(["SGAS", "XMF2"], properties):
         gasless = _identify_gas_less_cells(properties["SGAS"], properties["XMF2"])
     else:
-        error_text = "CO2 containment calculation failed. Cannot find necessary properties "
+        error_text = "CO2 containment calculation failed. Cannot find required properties "
         error_text += "SGAS+AMFG or SGAS+XMF2."
         raise RuntimeError(error_text)
     global_active_idx = active[~gasless]
@@ -291,7 +291,7 @@ def _eclipse_co2_simple_volume(source_data: SourceData) -> Dict:
     return co2_vol_st1
 
 
-def _calculate_co2_from_source_data(
+def _calculate_co2_data_from_source_data(
         source_data: SourceData,
         calc_type: CalculationType,
         co2_molar_mass: float = DEFAULT_CO2_MOLAR_MASS,
@@ -308,10 +308,10 @@ def _calculate_co2_from_source_data(
         elif _is_subset(["RPORV", "BGAS", "BWAT", "XMF2", "YMF2"], active_props):
             source = "Eclipse"
         else:
-            error_text = "Lacking some properties to compute CO2 mass/volume"
+            error_text = "Lacking some required properties to compute CO2 mass/volume"
             raise RuntimeError(error_text)
     else:
-        error_text = "Lacking some properties to compute CO2 mass/volume"
+        error_text = "Lacking some required properties to compute CO2 mass/volume"
         error_text += "\nMissing: SGAS"
         raise RuntimeError(error_text)
 
@@ -368,45 +368,44 @@ def _calculate_co2_from_source_data(
                 )
         else:
             co2_amount = co2_mass_output
+    elif calc_type == CalculationType.volume_extent:
+        props_idx = np.where([getattr(source_data, x) is not None for x in props_check])[0]
+        props_names = [props_check[i] for i in props_idx]
+        plume_props_names = [x for x in props_names if x in ["SGAS", "AMFG", "XMF2"]]
+        properties = {x: getattr(source_data, x) for x in plume_props_names}
+        inactive_gas_cells = {x: _identify_gas_less_cells({x: properties[plume_props_names[0]][x]},
+                                                          {x: properties[plume_props_names[1]][x]})
+                                for x in source_data.DATES}
+        vols_ext = {t: np.array([0] * len(source_data.VOL[t])) for t in source_data.DATES}
+        for t in source_data.DATES:
+            vols_ext[t][~inactive_gas_cells[t]] = np.array(source_data.VOL[t])[~inactive_gas_cells[t]]
+        co2_amount = Co2Data(
+              source_data.x,
+              source_data.y,
+              [Co2DataAtTimeStep(
+                  t,
+                  None,
+                  None,
+                  np.array(vols_ext[t])
+              ) for t in vols_ext],
+              "m3",
+              source_data.zone
+          )
     else:
-        if calc_type == CalculationType.volume_extent:
-            props_idx = np.where([getattr(source_data, x) is not None for x in props_check])[0]
-            props_names = [props_check[i] for i in props_idx]
-            plume_props_names = [x for x in props_names if x in ["SGAS", "AMFG", "XMF2"]]
-            properties = {x: getattr(source_data, x) for x in plume_props_names}
-            inactive_gas_cells = {x: _identify_gas_less_cells({x: properties[plume_props_names[0]][x]},
-                                                              {x: properties[plume_props_names[1]][x]})
-                                    for x in source_data.DATES}
-            vols_ext = {t: np.array([0] * len(source_data.VOL[t])) for t in source_data.DATES}
-            for t in source_data.DATES:
-                vols_ext[t][~inactive_gas_cells[t]] = np.array(source_data.VOL[t])[~inactive_gas_cells[t]]
-            co2_amount = Co2Data(
-                  source_data.x,
-                  source_data.y,
-                  [Co2DataAtTimeStep(
-                      t,
-                      None,
-                      None,
-                      np.array(vols_ext[t])
-                  ) for t in vols_ext],
-                  "m3",
-                  source_data.zone
-              )
+        if source == "PFlotran":
+            vols_co2 = _pflotran_co2_simple_volume(source_data)
         else:
-            if source == "PFlotran":
-                vols_co2 = _pflotran_co2_simple_volume(source_data)
-            else:
-                vols_co2 = _eclipse_co2_simple_volume(source_data)
-            vols_co2_simp = {t: [vols_co2[t][0], vols_co2[t][1]] for t in vols_co2}
-            co2_amount = Co2Data(source_data.x,
-                                 source_data.y,
-                                 [Co2DataAtTimeStep(
-                                     t,
-                                     np.array(vols_co2_simp[t][0]),
-                                     np.array(vols_co2_simp[t][1]),
-                                     None) for t in vols_co2_simp],
-                                 "m3",
-                                 source_data.zone)
+            vols_co2 = _eclipse_co2_simple_volume(source_data)
+        vols_co2_simp = {t: [vols_co2[t][0], vols_co2[t][1]] for t in vols_co2}
+        co2_amount = Co2Data(source_data.x,
+                             source_data.y,
+                             [Co2DataAtTimeStep(
+                                 t,
+                                 np.array(vols_co2_simp[t][0]),
+                                 np.array(vols_co2_simp[t][1]),
+                                 None) for t in vols_co2_simp],
+                             "m3",
+                             source_data.zone)
     return co2_amount
 
 
@@ -420,12 +419,12 @@ def calculate_co2(
     source_data = _extract_source_data(
         grid_file,
         unrst_file,
-        PROPERTIES_USED_FOR_CALCULATIONS,
+        PROPERTIES_TO_EXTRACT,
         init_file,
         zone_file
     )
     calc_type = _set_calc_type_from_input_string(calc_type_input.lower())
-    co2_data = _calculate_co2_from_source_data(source_data, calc_type=calc_type)
+    co2_data = _calculate_co2_data_from_source_data(source_data, calc_type=calc_type)
     return co2_data
 
 
